58bd26ec6666f2be881030833c49d70d4e830e92
gleicy.faustino <gleicy.faustino@cidades.gov.br>
2017-06-09 18:24:02 -0300

    impressão relatorio

--- Horus/src/main/java/br/com/horus/bean/ProdutoBean.java
+++ Horus/src/main/java/br/com/horus/bean/ProdutoBean.java
@@ -1,19 +1,29 @@
 package br.com.horus.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import org.omnifaces.util.Faces;
 import org.omnifaces.util.Messages;
+import org.primefaces.component.datatable.DataTable;
 
 import br.com.horus.dao.MarcaDAO;
 import br.com.horus.dao.ProdutoDAO;
 import br.com.horus.model.Marca;
 import br.com.horus.model.Produto;
+import br.com.horus.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -109,5 +119,37 @@ public class ProdutoBean implements Serializable{
 		}
 	}
 
+	public void imprimir() {
+		
+		try {
+			DataTable tabela = (DataTable) Faces.getViewRoot().findComponent("listagemProdutos:tabelaProdutos");
+			Map<String, Object> filtros = tabela.getFilters();
 
+			String proDescricao = (String) filtros.get("nome");
+			String fabDescricao = (String) filtros.get("marca.nome_marca");
+
+			String caminho = Faces.getRealPath("/reports/produtos.jasper");
+
+			Map<String, Object> parametros = new HashMap<>();
+			if (proDescricao == null) {
+				parametros.put("PRODUTO_NOME", "%%");
+			} else {
+				parametros.put("PRODUTO_NOME", "%" + proDescricao + "%");
+			}
+			if (fabDescricao == null) {
+				parametros.put("MARCA_NOME", "%%");
+			} else {
+				parametros.put("MARCA_NOME", "%" + fabDescricao + "%");
+			}
+
+			Connection conexao = HibernateUtil.getConexao();
+
+			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+
+			JasperPrintManager.printReport(relatorio, true);
+		} catch (JRException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar o relatório");
+			erro.printStackTrace();
+		}
+	}
 }
